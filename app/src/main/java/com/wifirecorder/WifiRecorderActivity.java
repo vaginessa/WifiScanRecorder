ce354ae087b6664a3e6f37bcd85691a78ae9ba2f
lijinglong-thu <lijinlon@gmail.com>
2015-10-26 17:43:51 +0800

    1.调整了记录的方式，现在可以在记录中添加后面扫描到的与前面不重复的AP，扫描不到的记为-100； 2.添加了wifi分布的按钮，但是记录可以，读取还是有问题，以后再想办法解决问题，考虑arraylist之类的。

--- app/src/main/java/com/wifirecorder/WifiRecorderActivity.java
+++ app/src/main/java/com/wifirecorder/WifiRecorderActivity.java
@@ -1,9 +1,14 @@
 package com.wifirecorder;
 
 import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Timer;
@@ -18,6 +23,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.ConnectivityManager;
@@ -27,6 +33,7 @@ import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -51,23 +58,25 @@ public class WifiRecorderActivity extends Activity
 	private Button btnStop;
 	private Button btnStart;//记录wifi资讯
 	private Button btnExit;
+	private Button btnSaveDefault;
+	private Button btnReadDefault;
 	private TextView wifiData;//界面显示wifi数据
 	private TextView scanProgress;//界面显示wifi数据
 	private TextView viewScanNum;
 	private TextView viewDelayNum;
+	private EditText edtBase;
 	private EditText editScanNum;
 	private EditText eidtDelayNum;
     private ProgressBar scanPb;//扫描进度
 	private WifiManager wifiManager;//管理并控制wifi
 	private File wifiRecFile;//存放wifi数据的文件名
-	private Timer wifiTimer;
-	//private WifiBroadcastReceiver wifiScanReciver;
+	private String baseFile;//存放配置文件的名称
 	private ConnectivityManager connectivityManager;
 	private int scanCount=0;//扫描次数
 	private boolean isScan=false;//是否开始采集，record按下时变为true
 	private int APSum = 20;//预先计划记录的AP总数
 	private List<ScanResult> firstScan;//第一次扫描结果
-    public Thread thScan;
+	private List<ScanResult> baseScan;//扫描结果汇总，作为存储的基准
 
 	private int scans = SCANS_DEFAULT;
 	private int delay = DELAY_DEFAULT;
@@ -81,26 +90,29 @@ public class WifiRecorderActivity extends Activity
 		btnStop = (Button)findViewById(R.id.btnStop);
 		btnStart = (Button)findViewById(R.id.btnStart);
 		btnExit = (Button)findViewById(R.id.btnExit);
+		btnSaveDefault = (Button)findViewById(R.id.btnSaveDefault);
+		btnReadDefault = (Button)findViewById(R.id.btnReadDefault);
 		wifiData = (TextView)findViewById(R.id.wifiData);
 		scanProgress = (TextView)findViewById(R.id.ScanProgress);
 		viewScanNum = (TextView)findViewById(R.id.viewScanNum);
 		viewDelayNum = (TextView)findViewById(R.id.viewDelayNum);
 		editScanNum = (EditText)findViewById(R.id.editScanNum);
 		eidtDelayNum = (EditText)findViewById(R.id.editDelayNum);
+		edtBase = (EditText)findViewById(R.id.edtBase);
         scanPb = (ProgressBar)findViewById(R.id.scanPb);
 		//设定wifi装置
 		wifiManager = (WifiManager)this.getSystemService(Context.WIFI_SERVICE);//取得WifiManager
-		wifiTimer = new Timer(WIFI_TIMER); //Timer
+		//wifiTimer = new Timer(WIFI_TIMER); //Timer
 		//wifiScanReciver = new WifiBroadcastReceiver(); //
 		connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
 		//启用wifi装置
 		doStartWifi();
-		//取得wifi列表
-		//GetWifiList();
-		//设定按钮功能
+
 		btnStart.setOnClickListener(btnListener);
 		btnStop.setOnClickListener(btnListener);
 		btnExit.setOnClickListener(btnListener);
+		btnSaveDefault.setOnClickListener(btnListener);
+		btnReadDefault.setOnClickListener(btnListener);
 
 		btnStart.setEnabled(true);
 		btnStop.setEnabled(false);
@@ -119,25 +131,40 @@ public class WifiRecorderActivity extends Activity
 					doStopScan();
 					break;
 				case R.id.btnStart:
-					RecordCheckWindow();
+					Log.d(TAG, "Starting record");
+					try {
+						doStartScan();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 					break;
 				case R.id.btnExit:
 					doStopWifi();
 					finish();
 					break;
+				case R.id.btnSaveDefault:
+					baseFile = edtBase.getText().toString();
+					if (baseFile!=null){
+					doDefaultSave(baseFile,baseScan);
+					}
+					else{
+						doNotify("请输入分布名称");
+					}
+					break;
+				case R.id.btnReadDefault:
+					baseFile = edtBase.getText().toString();
+					if (baseFile!=null){
+						baseScan = doDefaultRead(baseFile);
+						baseFile = edtBase.getText().toString();
+					}
+					else{
+						doNotify("请输入分布名称");
+					}
+					break;
 			}
 		}
 	};
 
-	private void RecordCheckWindow()
-	{
-		Log.d(TAG, "Starting record");
-				try {
-					doStartScan();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-	}
 
 	public void CreatFile() throws Exception
 	{
@@ -160,8 +187,8 @@ public class WifiRecorderActivity extends Activity
 		long timestamp = System.currentTimeMillis() / 1000;
 		stringBuilder.append(String.format("%s ", timestamp));//记录扫描wifi序号，时间
 		if (scanCount == 1){
-			firstScan = scanResults;
-			for (ScanResult firstscanResult : firstScan) {
+			baseScan = scanResults;
+			for (ScanResult firstscanResult : baseScan) {
                 stringBuilder.append(String.format("%s ",firstscanResult.level));//记录wifi信息
             }
 			stringBuilder.append("\n");
@@ -169,18 +196,31 @@ public class WifiRecorderActivity extends Activity
 			//wifiData.setText(String.format("%s", stringBuilder));
 		}
 		else {
+			//完善baseScan信息
+			for (ScanResult scanResult : scanResults) {
+				boolean wasScan = false;
+				for (ScanResult baseResult : baseScan){
+					if (scanResult.BSSID.equals(baseResult.BSSID)){
+						wasScan = true;
+						break;
+					}
+				}
+				if (!wasScan){//wasScan 为false，baseScan未检测到
+					baseScan.add(scanResult);//则添加该wifi信息至baseScan
+				}
+			}
 			//利用循环搜索与第一次搜索结果进行匹配。
-			for (ScanResult firstscanResult : firstScan){
+			for (ScanResult baseResult : baseScan){
 				boolean isCheck = false;//匹配是否存在，匹配到为true
 				for (ScanResult scanResult : scanResults) {
-					if (firstscanResult.BSSID.equals(scanResult.BSSID)){
+					if (scanResult.BSSID.equals(baseResult.BSSID)){
 						stringBuilder.append(String.format("%s ", scanResult.level));//记录wifi信息
 						isCheck = true;
                         break;
                     }
                 }
                 if (!isCheck){
-					stringBuilder.append("-110 ");
+					stringBuilder.append("-100 ");//某次未扫描到到,设置为-100
                 }
             }
 			stringBuilder.append("\n");
@@ -326,5 +366,78 @@ public class WifiRecorderActivity extends Activity
             super.onPostExecute(result);
         }
     }
+
+	@SuppressWarnings("unchecked")
+	public static List<ScanResult> String2WifiList(String WifiListString)
+			throws StreamCorruptedException, IOException,
+			ClassNotFoundException {
+		byte[] mobileBytes = Base64.decode(WifiListString.getBytes(),Base64.DEFAULT);
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(mobileBytes);
+		ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+		List<ScanResult> WifiList = (List<ScanResult>) objectInputStream.readObject();
+		objectInputStream.close();
+		return WifiList;
+	}
+	//SharedPreferences保存List集合
+	public void doDefaultSave(String FileName,List<ScanResult> scanResults){
+		Toast.makeText(getApplicationContext(),"save starting!",Toast.LENGTH_LONG).show();
+		// 实例化一个ByteArrayOutputStream对象，用来装载压缩后的字节文件。
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		// 然后将得到的字符数据装载到ObjectOutputStream
+		ObjectOutputStream objectOutputStream = null;
+		try{
+			objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+			objectOutputStream.writeObject(scanResults);
+			byteArrayOutputStream.close();
+			objectOutputStream.close();
+		}catch(Exception e){
+			try {
+				byteArrayOutputStream.close();
+				objectOutputStream.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		// writeObject 方法负责写入特定类的对象的状态，以便相应的 readObject 方法可以还原它
+		String WifiListString = null;
+		// 最后，用Base64.encode将字节文件转换成Base64编码保存在String中
+		try {
+			WifiListString = new String(Base64.encode(byteArrayOutputStream.toByteArray(), Base64.DEFAULT));
+			Toast.makeText(getApplicationContext(),"save successfully!",Toast.LENGTH_LONG).show();
+		}catch (Exception e){
+			Toast.makeText(getApplicationContext(),"save failed!",Toast.LENGTH_LONG).show();
+		}
+			SharedPreferences sharedPreferences= getSharedPreferences(FileName, Context.MODE_PRIVATE);
+			SharedPreferences.Editor edit = sharedPreferences.edit();
+			edit.putString("wifibase",WifiListString);
+			edit.commit();
+	}
+	@SuppressWarnings("unchecked")
+	public List<ScanResult> doDefaultRead(String FileName){
+		Toast.makeText(getApplicationContext(),"read starting!",Toast.LENGTH_LONG).show();
+		SharedPreferences sharedPreferences = getSharedPreferences(FileName, Context.MODE_PRIVATE);
+		String liststr = sharedPreferences.getString("wifibase", "");
+		List<ScanResult> wifiList = null;
+		//转换文本数据为二进制数据
+		byte[] mobileBytes = Base64.decode(liststr.getBytes(), Base64.DEFAULT);
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(mobileBytes);
+		ObjectInputStream objectInputStream = null;
+		try {
+			objectInputStream = new ObjectInputStream(byteArrayInputStream);
+			wifiList = (List<ScanResult>)objectInputStream.readObject();
+			byteArrayInputStream.close();
+			objectInputStream.close();
+		}catch (Exception e){
+			try {
+				byteArrayInputStream.close();
+				objectInputStream.close();
+			}catch (IOException e1){
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		return wifiList;
+	}
 
 }
